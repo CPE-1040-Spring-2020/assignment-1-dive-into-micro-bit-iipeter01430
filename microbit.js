04086ced5179ed585c2151521fe7f00f3d90f567
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-01-27 18:29:35 -0700

    Update microbit.js

--- microbit.js
+++ microbit.js
@@ -1,5 +1,23 @@
+let IconHeart: boolean = false
+
+
+input.onButtonPressed(Button.A, function () {
+    if (IconHeart ) {
+        IconHeart=false
+    } else {
+        IconHeart=true
+    }
+})
+
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    if (IconHeart) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.Cow)
+    }
+
+
     basic.pause(500)
     basic.clearScreen()
+
 })
